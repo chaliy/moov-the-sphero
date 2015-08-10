75ea7321ccca2f7a22f93cdb81b3cda8990cbc37
Mike Chaliy <mike@chaliy.name>
2015-08-10 17:16:28 +0300

    MoovTheSphero

---
+++ MoovTheSphero/Moov/MoovService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Reflection;
+using Moov;
+
+namespace MoovTheSphero.Moov
+{
+    public class MoovService
+    {
+        private readonly string _name;
+        readonly Subject<SensorsDataEventArgs> _sensors = new Subject<SensorsDataEventArgs>();
+
+        public MoovService(string name = "Moov")
+        {
+            _name = name;
+        }
+
+        private enum GyroscopeAxis
+        {
+            X = 1,
+            Y = 2,
+            XY = 3,
+            Z = 4,
+            XZ = 5,
+            YZ = 6,
+            XYZ = 7
+        }
+
+        public IObservable<SensorsDataEventArgs> Sensors => _sensors;
+
+        public void Start()
+        {
+            var device = new Device();
+            device.GetType().GetField("deviceName", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(device, _name);
+
+            device.OnDeviceDiscovered += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Discovered: " + eventArgs.Discovered + " C:" + device.IsConnected + ";R:" + device.IsRunning);
+                // Enable Girocope
+                device.SetSensorsConf((byte)GyroscopeAxis.XYZ);
+            };
+
+            device.OnMotiStat += (sender, eventArgs) => Console.WriteLine("OnMotiStat");
+            device.OnSensorsDataAvailable += (sender, eventArgs) =>
+            {                
+                _sensors.OnNext(eventArgs);
+            };
+            device.OnKeyEvent += (sender, eventArgs) => Console.WriteLine("OnKeyEvent");
+
+            device.Discover();
+        }
+    }
+}
---
+++ MoovTheSphero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MoovTheSphero.Moov;
+
+namespace MoovTheSphero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moov = new MoovService("CyanMoov");
+
+            moov.Sensors.Subscribe(e =>
+            {
+                Console.WriteLine($"OnSensors: G: X: {e.GyroscopeX}, Y: {e.GyroscopeY}, Z: {e.GyroscopeZ}; " +
+                                  $"A: X: {e.AccelerationX}, Y: {e.AccelerationY}, Z: {e.AccelerationZ}");
+            });
+
+            moov.Start();
+
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
